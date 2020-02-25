220ddcdfcaafc451f85921afe095d4813b22a13f
suyoung <suyoung@DESKTOP-7J7DGTN>
2020-02-25 16:29:09 +0900

    my test

---
+++ hello_server/src/test/java/com/develop/test/user/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.test.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
